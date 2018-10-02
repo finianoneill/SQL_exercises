4bfe5d395b91abb968186e4f7aec77ea7ee6146a
Finian O'Neill <finian.d.oneill@gmail.com>
2018-10-02 07:56:03 -0700

    Add on to exercise 7.

--- sakila_SQL_exercises/sakila_SQL_exercise_7.sql
+++ sakila_SQL_exercises/sakila_SQL_exercise_7.sql
@@ -33,3 +33,9 @@ LEFT JOIN address a ON c.address_id = a.address_id) cust_address
 ON cit.city_id = cust_address.city_id
 WHERE cit.country_id IN (SELECT country_id FROM country
 WHERE country = "Canada");
+
+-- 7d;
+SELECT title FROM film
+WHERE film_id IN (SELECT film_id FROM film_category
+WHERE category_id IN (SELECT category_id FROM category
+WHERE name = "Family"));
